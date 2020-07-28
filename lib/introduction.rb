bb8b51a02b7ea70162b3d4f4c933a96c2e56f9c5
Eric Fiutak <fiutake14@gmail.com>
2020-07-28 01:30:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name}, I am a #{language} developer."
+  puts "Hi, my name is #{name}, I am learning to program in #{language}."
 end
